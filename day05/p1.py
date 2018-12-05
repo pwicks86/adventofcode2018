374cb9f002dcf08a0361c61cfb6c48b61737f1bb
Paul Wicks <pwicks86@gmail.com>
2018-12-04 22:05:33 -0800

    slight efficiency increase

--- day05/p1.py
+++ day05/p1.py
@@ -1,15 +1,16 @@
 f = open("input.txt", "r")
-poly = [c for c in f.read().strip()]
+poly = [ord(c) for c in f.read().strip()]
 print(poly)
 start_index = 0
 
 def compress():
     global poly
     global start_index
-    print(len(poly))
+    if len(poly) % 1000 == 0:
+        print(len(poly))
     for i in range(start_index, len(poly) - 1):
         pair = poly[i:i+2]
-        if pair[0] != pair[1] and pair[0].lower() == pair[1].lower():
+        if abs(pair[0] - pair[1]) == 32:
             poly = poly[:i] + poly[i+2:]
             return True
     return False
